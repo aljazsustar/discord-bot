591a4b589b2d6e0a30aec6931522b2fcd5bbc88e
aljaz <aljazsustar99@gmail,com>
2019-12-01 14:54:28 +0100

    Dad jokes update

--- bot.js
+++ bot.js
@@ -1,44 +1,68 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const https = require('https');
 
-var message = "";
+let message = "";
 
 client.on('ready', () => {
  console.log(`Logged in as ${client.user.tag}!`);
  });
 
 client.on('message', msg => {
- if (msg.content === 'ping') {
- msg.reply('pong');
- }
- });
- 
+    if (msg.content === "dad joke") {
+        https.get('https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/jokes', (response) => {
+            let data = '';
+            response.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            response.on('end', () => {
+                let object = JSON.parse(data);
+                msg.reply(object.setup + " " + object.punchline);
+            });
+        });
+    } else if (msg.content === "programming dad joke") {
+        https.get('https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/type/programming', (response) => {
+            let data = '';
+            response.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            response.on('end', () => {
+                let object = JSON.parse(data);
+                msg.reply(object[0].setup + " " + object[0].punchline);
+            });
+        });
+    }
+});
+
+
 client.on('message', msg =>{
-    if (msg.content == 'Marco'){
+    if (msg.content === 'Marco') {
         msg.reply('Polo!');
     }
-})
+});
 
 client.on('message', msg =>{
-    if (msg.content == 'lol'){
+    if (msg.content === 'lol') {
         msg.reply('BadJoke++;');
     }
-})
+});
 
 client.on('message', msg =>{
-    if (msg.content == 'hehe'){
+    if (msg.content === 'hehe') {
         msg.reply('VeryGoodJoke++;');
     }
-})
+});
 
 client.on('message', msg =>{
-    if (msg.content == 'jabolj play despacito'){
+    if (msg.content === 'jabolj play despacito') {
         msg.reply('https://www.youtube.com/watch?v=kJQP7kiw5Fk');
     }
-})
+});
 
 client.on('message', msg =>{
-    if (msg.content.substring(0,11) == 'jabolj play'){
+    if (msg.content.substring(0, 11) === 'jabolj play') {
         message = msg.content;
         console.log(message);
         var query = message.substring(12, message.length);
@@ -48,6 +72,18 @@ client.on('message', msg =>{
         var reply = 'https://www.youtube.com/results?search_query=' + formatted;
         msg.reply(reply);
     }
-})  
+});
+
+function fetchJoke(callback) {
+    let request = new XMLHttpRequest();
+    request.open('GET', 'https://us-central1-dadsofunny.cloudfunctions.net/DadJokes//random/jokes', true);
+    request.send();
+    request.onreadystatechange = function () {
+        if (request.status === "200") {
+            let json = JSON.parse(request.responseText);
+            callback(json);
+        }
+    };
+}
 
 client.login('NTYzMTI1NzkyMDIwNzU4NTI4.XKU46g.dFleGkHP3CoAX9jZSje23pvgjdM');
