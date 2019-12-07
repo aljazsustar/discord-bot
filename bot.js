9392dff4171e81078e443606f958790bb65f064c
aljaz <aljazsustar99@gmail,com>
2019-12-07 16:08:05 +0100

    Jabolj inspirational quotes added

--- bot.js
+++ bot.js
@@ -7,44 +7,43 @@ let message = "";
 let rule = new schedule.RecurrenceRule();
 rule.hour = 7;
 
-let job = schedule.scheduleJob(rule, () => {
-    https.get('https://api.kanye.rest', (response) => {
+let job = schedule.scheduleJob(rule, function () {
+    https.get('https://api.kanye.rest/?format=text', (response) => {
         let data = '';
         response.on('data', (chunk) => {
             data += chunk;
         });
         response.on('end', () => {
-            let object = JSON.stringify(data);
-            client.channels.get('651822721457586188').send(object.quote);
+            client.channels.get('651822721457586188').send(data);
         })
     })
 });
 
-client.on('message', msg =>{
+client.on('message', msg => {
     if (msg.content === 'Marco') {
         msg.reply('Polo!');
     }
 });
 
-client.on('message', msg =>{
+client.on('message', msg => {
     if (msg.content === 'lol') {
         msg.reply('BadJoke++;');
     }
 });
 
-client.on('message', msg =>{
+client.on('message', msg => {
     if (msg.content === 'hehe') {
         msg.reply('VeryGoodJoke++;');
     }
 });
 
-client.on('message', msg =>{
+client.on('message', msg => {
     if (msg.content === 'jabolj play despacito') {
         msg.reply('https://www.youtube.com/watch?v=kJQP7kiw5Fk');
     }
 });
 
-client.on('message', msg =>{
+client.on('message', msg => {
     if (msg.content.substring(0, 11) === 'jabolj play') {
         message = msg.content;
         console.log(message);
---
+++ nbaBot.js
@@ -0,0 +1,6 @@
+const Discord = require('discord.js');
+
+
+const client = new Discord.Client();
+
+client.login('NjUyODU3NzA1MTMxNjcxNTYy.XeujiQ.XBDHGFLsMWo_vB8xrREjUe8c3aE')
