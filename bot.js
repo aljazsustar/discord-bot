0a18ef1c07adda586c498e3fb40b5846156377c1
aljaz <aljazful@gmail.com>
2020-07-04 22:50:40 +0200

    reminder functionality

--- bot.js
+++ bot.js
@@ -20,28 +20,64 @@ let motivationJob = schedule.scheduleJob(rule, function () {
     })
 });
 
-client.on('message', msg => {
+client.on('message', async msg => {
     if (msg.content === 'Marco') {
         msg.reply('Polo!');
     }
-});
-
-client.on('message', msg => {
     if (msg.content === 'jabolj play despacito') {
         msg.reply('https://www.youtube.com/watch?v=kJQP7kiw5Fk');
     }
-});
 
-client.on('message', msg => {
-    if (msg.content.substring(0, 11) === 'jabolj play') {
-        message = msg.content;
-        console.log(message);
-        var query = message.substring(12, message.length);
-        console.log(query);
-        var formatted = query.replace(/ /gi, "+");
-        console.log(formatted);
-        var reply = 'https://www.youtube.com/results?search_query=' + formatted;
-        msg.reply(reply);
+    if (msg.content.startsWith('!remind')) {
+        remind(msg);
     }
 });
+
+
+function remind(msg) {
+    let params = parse(msg);
+    console.log(params);
+    if ((params[0] && params[1] && params[2]) <= 0) {
+        msg.reply('Wait, that\'s illegal!');
+        return;
+    }
+    let reminder_rule = schedule.scheduleJob(setDate(params[0], params[1], params[2]), () => {
+        msg.reply(params[3]);
+    });
+
+}
+
+function parse(msg) {
+    let raw = msg.content.substring(7);
+    let days = parseInt(raw.substring(0, raw.indexOf('d')));
+    let hours = parseInt(raw.substring(raw.indexOf('d') + 1, raw.indexOf('h')));
+    let minutes = parseInt(raw.substring(raw.indexOf('h') + 1, raw.indexOf('m')));
+    let message = raw.substring(raw.indexOf('m') + 2);
+    return [days, hours, minutes, message]
+}
+
+function addDays(date, days) {
+    date.setDate(date.getDate() + days);
+    return date;
+}
+
+function addHours(date, hours) {
+    date.setHours(date.getHours() + hours);
+    return date;
+}
+
+function addMinutes(date, minutes) {
+    date.setMinutes(date.getMinutes() + minutes);
+    return date;
+}
+
+function setDate(d, h, m) {
+    let date = new Date();
+    let days = addDays(date, d);
+    let hours = addHours(days, h);
+    console.log(addMinutes(hours, m));
+    return addMinutes(hours, m);
+}
+
+
 client.login('NTYzMTI1NzkyMDIwNzU4NTI4.XKU46g.dFleGkHP3CoAX9jZSje23pvgjdM');
